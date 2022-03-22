0903e0fa00e6dbe6bd645367a07b82b42cdd76fe
Robert Granja <robeertgr@live.com>
2022-03-22 10:20:41 -0300

    Criação do componente cores das notas

---
+++ src/styles/styleCores.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from 'react-native'
+
+export default StyleSheet.create({
+    Basico: {
+        flexGrow: 1,
+        alignItems: "center",
+        padding: 20,
+        fontSize: 14,
+        textAlign: "center",
+        backgroundColor: "#F8F8F8"
+    },
+    Rosa: {
+        flexGrow: 1,
+        alignItems: "center",
+        padding: 20,
+        fontSize: 14,
+        textAlign: "center",
+        backgroundColor: "#FFF3F3"
+    },
+    Azul: {
+        flexGrow: 1,
+        alignItems: "center",
+        padding: 20,
+        fontSize: 14,
+        textAlign: "center",
+        backgroundColor: "#EAF1FF"
+    },
+    VerdeAgua: {
+        flexGrow: 1,
+        alignItems: "center",
+        padding: 20,
+        fontSize: 14,
+        textAlign: "center",
+        backgroundColor: "#E4FFEF"
+    }
+})
