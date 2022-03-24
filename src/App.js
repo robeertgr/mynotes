b6ec71c36e033cd32956cc31771c8e82be6c542c
Robert Granja <robeertgr@live.com>
2022-03-24 15:40:51 -0300

    Desenvolvimento da pagina criar notas

--- src/App.js
+++ src/App.js
@@ -1,48 +1,51 @@
+import { Picker } from '@react-native-picker/picker';
 import React, { Component } from 'react'
-import { View, SafeAreaView, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, SafeAreaView, Text, Image, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
 
 class App extends Component {
     render() {
 
+        
+
         return (
             <SafeAreaView>
                 <View style={styles.container}>
-                    <Text style={styles.textNotas}>
-                        Notas
+                    <Text style={styles.txtCriarNota}>
+                        Criar nota
                     </Text>
                     <TouchableOpacity>
                         <Image
-                            source={require('./assets/ButtonSearch.png')}
-                            style={styles.imgSearch}
+                            source={require('./assets/ButtonClose.png')}
+                            style={styles.btnClose}
                         />
                     </TouchableOpacity>
-                    <TouchableOpacity>
-                        <Image
-                            source={require('./assets/ButtonAdd.png')}
-                            style={styles.imgAdd}
-                        />
-                    </TouchableOpacity>
-                    <Image
-                        source={require('./assets/NoNotesMainPage.png')}
-                        style={styles.imgNoNotes}
-                    />
-                </View>
-                <View>
-                    <Text style={styles.txtNoNotes}>
-                        Não tem nenhuma nota aqui
+                </View >
+                
+                <View style={{backgroundColor: '#EFEFEF'}}>
+                    <Text style={styles.txtNomeNota}>
+                        Nota da nota (obrigatório)
                     </Text>
-                    <Text style={styles.txtNotesHere}>
-                        Crie notas e você poderá vê-las aqui.
+
+                    <TextInput style={styles.inputNoteName} placeholder='Insira'></TextInput>
+
+                    <Text style={styles.txtNomeNota}>
+                        Descrição
                     </Text>
-                </View>
-                <View>
-                    <TouchableOpacity>
-                        <Image
-                            source={require('./assets/ButtonAdd.png')}
-                            style={styles.imgAddBotton}
-                        />
-                    </TouchableOpacity>
-                </View>
+                    <TextInput style={styles.inputNoteDescription} placeholder='Insira' multiline numberOfLines={5}></TextInput>
+
+                    <Text style={styles.txtNomeNota}>
+                        Prioridade
+                    </Text>
+                    <Picker style={styles.pickerPriority} placeholder='Escolha'>
+                        <Picker.Item key={1} value={1} label={"Urgente"}/>
+                        <Picker.Item key={2} value={2} label={"Alta"} />
+                        <Picker.Item key={3} value={3} label={"Média"} />
+                        <Picker.Item key={4} value={4} label={"Baixa"} />
+                    </Picker>
+                        
+                    
+                </View> 
+                
             </SafeAreaView>
         )
     }
@@ -51,57 +54,59 @@ class App extends Component {
 const styles = StyleSheet.create({
     container: {
         width: 415,
-        height: 132,
+        height: 72,
         backgroundColor: '#0F62FE',
         flexDirection: 'row'
     },
-    textNotas: {
+    txtCriarNota: {
         fontFamily: 'IBM Plex Sans',
         fontSize: 32,
         color: '#FFFFFF',
-        marginTop: 73,
+        marginTop: 25,
         marginLeft: 16
     },
-    imgSearch: {
-        width: 25,
-        height: 25,
-        marginLeft: 220,
-        marginTop: 90,
+    btnClose: {
+        width: 60,
+        height: 60,
+        marginLeft: 190,
+        marginTop: 70,
         display: 'flex'
     },
-    imgAdd: {
-        width: 40,
-        height: 40,
-        marginLeft: 10,
-        marginTop: 83,
-        display: 'flex'
-    },
-    imgNoNotes: {
-        width: 91,
-        height: 128,
-        marginLeft: -380,
-        marginTop: 180,
-        display: 'flex'
-    },
-    txtNoNotes: {
+    txtNomeNota:{
         fontFamily: 'IBM Plex Sans',
         fontSize: 20,
         color: '#161616',
-        marginTop: 200,
-        marginLeft: 16
+        marginTop: 20,
+        marginLeft: 16,
+        fontWeight: 'bold'
     },
-    txtNotesHere: {
+    inputNoteName:{
+        backgroundColor: '#FFF',
+        borderBottomWidth: 1,
+        marginTop: 10,
+        marginLeft: 16,
+        marginRight: 16,
+        fontSize: 20,
+        padding: 10
+    },
+    inputNoteDescription:{
+        backgroundColor: '#FFF',
+        borderBottomWidth: 1,
+        fontSize: 20,
+        padding: 10,
+        marginLeft: 16,
+        marginRight: 16,
+        marginTop: 10 
+    },
+    pickerPriority: {
+        backgroundColor: '#FFF',
         fontFamily: 'IBM Plex Sans',
-        fontSize: 14,
-        color: '#8D8D8D',
-        marginLeft: 16
-    },
-    imgAddBotton: {
-        width: 40,
-        height: 40,
-        marginLeft: 360,
-        marginTop: 205,
-        display: 'flex'
+        fontSize: 20,
+        color: '#161616',
+        marginTop: 20,
+        marginRight: 16,
+        marginLeft: 16,
+        fontWeight: 'bold'
     }
 })
 
